fffe2a791aae34a714d39da8ecd99ef0c8302f9c
Omar <hroumtiomar11@gmail.com>
2025-10-21 10:16:49 +0100

    added to-do-list project

--- src/projects/to-do-list/render.jsx
+++ src/projects/to-do-list/render.jsx
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import Context from "./context";
 
 export default function Render(){
-    const {tasks, setToModify, removeTask} = useContext(Context)
+    const {tasks, setToModify, removeTask, toggleTask} = useContext(Context)
 
 
     return(
         <div className="tasksContainer">
             {!tasks||tasks.length===0?(<h2>No tasks found</h2>):tasks.map(t=>(
                 <div className="task-container" key={t.id}>
-                    <p className="task" style={{textDecoration:t.state==='Done'?'line-through':'none'}}>{t.task}</p>
+                    <p className="task" style={{textDecoration:t.state==='Done'?'line-through':'none'}} onClick={()=>toggleTask(t.id)}>{t.task}</p>
                     <span className="actions">
                         <button className="edit-btn" onClick={()=>setToModify(t.id)}>✏️</button>
                         <button className="remove-btn" onClick={()=>removeTask(t.id)}>🗑️</button>
